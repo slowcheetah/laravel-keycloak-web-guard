9b0250269a6fd03abf9f99865eb40b95315fad9f
Sergey Nechaev <slow_cheetah@inbox.ru>
2022-11-04 17:29:29 +0700

    Exclude login & callback routes from auth check middleware

--- src/Middleware/KeycloakAuthenticated.php
+++ src/Middleware/KeycloakAuthenticated.php
@@ -2,10 +2,29 @@
 
 namespace Vizir\KeycloakWebGuard\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate;
 
 class KeycloakAuthenticated extends Authenticate
 {
+    public function handle($request, Closure $next, ...$guards)
+    {
+        $excludedRoutes = [
+            route('keycloak.login', [],false),
+            route('keycloak.callback', [],false),
+        ];
+        $requestPath = '/' . $request->path();
+        foreach ($excludedRoutes as $excludedRoute) {
+            if ($requestPath === $excludedRoute) {
+                \Log::debug("Skipping {$excludedRoute} from auth check...");
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next, $guards);
+    }
+
+
     /**
      * Redirect user if it's not authenticated.
      *
